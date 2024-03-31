14050f722d3a335c8b036251cdf760c45c02338e
artzzon <nexus.artem@gmail.com>
2024-03-31 23:22:53 +0300

    make Filter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
+import Filter from "./components/Filter/Filter";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
       <Header />
+      <Filter />
     </div>
   );
 }
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import Button from "../UI/button/Button";
+import styles from "./Filter.module.scss";
 
 const Filter = () => {
-  return <div>Filter</div>;
+  return (
+    <div className={styles.filter}>
+      <div className={styles.filter__header}>
+        <img src="icons/filter_icon.svg" alt="" />
+        <h3>Filters</h3>
+      </div>
+      <div className={styles.filter__category_status}>
+        <div className={styles.filter__category}>
+          <h4>CATEGORY</h4>
+          <Button>All</Button>
+          <Button>Canola</Button>
+          <Button>Corn</Button>
+          <Button>Oats</Button>
+          <Button>Wheat</Button>
+          <Button>Soybeans</Button>
+          <Button>Barley</Button>
+        </div>
+        <div className={styles.filter__status}>
+          <h4>STATUS</h4>
+          <input type="checkbox" name="Limited" />
+          <span>Limited</span>
+          <input type="checkbox" name="New" />
+          <span>New</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
