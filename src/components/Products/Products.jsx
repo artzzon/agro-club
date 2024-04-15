62a9d226b232ca194a0b239ede1873dafeb95a4e
artzzon <nexus.artem@gmail.com>
2024-04-15 22:09:44 +0300

    add render products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,18 +1,28 @@
+import React from "react";
 import ProductCard from "../ProductCard/ProductCard";
-
+import Loader from "../UI/loader/Loader";
 import styles from "./Products.module.scss";
+import { useFetching } from "../../hooks/useFetching";
+import QueryService from "../../API/QueryService";
 
 const Products = () => {
+  const [products, setProducts] = React.useState([]);
+  const [tryFetch, isLoading, error] = useFetching(async () => {
+    const response = await QueryService.getAllProducts();
+    setProducts(response.data);
+  });
+
+  React.useEffect(() => {
+    tryFetch();
+  }, []);
+  console.log(products);
   return (
     <div className={styles.products}>
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      {isLoading ? (
+        <Loader width={150} />
+      ) : (
+        products.map((product) => <ProductCard key={product.id} {...product} />)
+      )}
     </div>
   );
 };
