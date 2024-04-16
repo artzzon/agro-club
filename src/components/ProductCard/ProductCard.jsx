206593b5f65e6eec994b0bf1c8b1e5965c1b2019
artzzon <nexus.artem@gmail.com>
2024-04-16 22:46:13 +0300

    fix layout product card with long text

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,30 +1,39 @@
 import styles from "./ProductCard.module.scss";
 
-const ProductCard = () => {
+const ProductCard = ({
+  categoryName,
+  description,
+  discount,
+  imgUrl,
+  isLimited,
+  isNew,
+  name,
+  price,
+}) => {
   return (
     <div className={styles.productCard}>
+      <div className={styles.productCard__badges}>
+        {isLimited && (
+          <span className={styles.productCard__badges_limited}>Limited</span>
+        )}
+        {isNew && <span className={styles.productCard__badges_new}>New</span>}
+      </div>
       <img src="https://i.ibb.co/m4xLJYR/Wheat-2x.png" alt="" width="116px" />
       <div className={styles.productCard__content}>
         <div className={styles.productCard__description}>
           <div className={styles.productCard__description_title}>
-            <span>Canola</span>
+            <span>{categoryName}</span>
           </div>
           <div className={styles.productCard__description_head}>
-            <h5>CS2300</h5>
+            <h5>{name}</h5>
           </div>
-          <p>Roundup Ready Canola Hybrid</p>
-          <div className={styles.productCard__description_badges}>
-            <span className={styles.productCard__description_badges_limited}>
-              Limited
-            </span>
-            <span className={styles.productCard__description_badges_new}>
-              New
-            </span>
+          <div className={styles.productCard__description_body}>
+            <p>{description}</p>
           </div>
         </div>
         <div className={styles.productCard__price}>
-          <p>$34.00</p>
-          <span>Discount $2 per bag</span>
+          <p>${price}</p>
+          {discount && <span>Discount ${discount} per bag</span>}
         </div>
       </div>
     </div>
