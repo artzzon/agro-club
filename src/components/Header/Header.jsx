f6d1b57e8e22222174e6f9c9da470f89b688a807
artzzon <nexus.artem@gmail.com>
2024-04-20 19:43:31 +0300

    add reload on click logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,9 @@ import styles from "./Header.module.scss";
 const Header = () => {
   return (
     <div className={styles.header}>
-      <h1 className={styles.header__logo}>Products</h1>
+      <h1 className={styles.header__logo} onClick={window.location.reload()}>
+        Products
+      </h1>
       <Input />
     </div>
   );
